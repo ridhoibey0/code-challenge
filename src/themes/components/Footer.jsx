0d09db2a9d5754765d9607853bbecf720f7fd53e
ridhoibey0 <ridhoibey@gmail.com>
2023-04-05 11:35:03 +0700

    Update Footer

--- src/themes/components/Footer.jsx
+++ src/themes/components/Footer.jsx
@@ -27,16 +27,26 @@ export const Footer = () => {
         <Flex justify="space-between" align="start" w="full">
           <Flex left="0" marginLeft="20px" h="100%">
             <VStack align="start">
-              <svg
-                width="41"
-                height="50"
-                viewBox="0 0 41 50"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M35 25L17.5 50L0 25L17.5 0" fill="#E1DFFC" />
-                <path d="M41 25L23.5 50L6 25L23.5 0" fill="#8C82FC" />
-              </svg>
+              <HStack>
+                <svg
+                  width="41"
+                  height="50"
+                  viewBox="0 0 41 50"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M35 25L17.5 50L0 25L17.5 0" fill="#E1DFFC" />
+                  <path d="M41 25L23.5 50L6 25L23.5 0" fill="#8C82FC" />
+                </svg>
+                <Text
+                  fontSize="18px"
+                  fontWeight={700}
+                  lineHeight="22.63px"
+                  color="secondary"
+                >
+                  Suxz
+                </Text>
+              </HStack>
               <Text>
                 Lorem ipsum dolor sit amet, <br></br> consectetur adipiscing
                 elit sed do eiusmod tempor <br></br> incididunt ut labore et
@@ -48,20 +58,23 @@ export const Footer = () => {
             <VStack align="start">
               <Heading fontSize="28px">Company</Heading>
               <Text>Home</Text>
-              <Text>Home</Text>
-              <Text>Home</Text>
+              <Text>Blogs</Text>
+              <Text>Careers</Text>
+              <Text>Portfolios</Text>
             </VStack>
             <VStack align="start">
               <Heading fontSize="28px">Links</Heading>
-              <Text>Home</Text>
-              <Text>Home</Text>
-              <Text>Home</Text>
+              <Text>Privacy & Policy</Text>
+              <Text>Github</Text>
+              <Text>Support</Text>
+              <Text>FAQs</Text>
             </VStack>
             <VStack align="start">
               <Heading fontSize="28px">Contact</Heading>
-              <Text>Home</Text>
-              <Text>Home</Text>
-              <Text>Home</Text>
+              <Text>
+                Jl. Narogong. Perum <br /> Cileungsi Elok Blok <br /> A No.5.
+                Jakarta Selatan. 12240
+              </Text>
             </VStack>
           </Flex>
         </Flex>
